c0c64d1a70163b5f6650dd2ef4619a78f93bad5e
= <=>
2025-03-14 06:28:04 -0300

    create start and reset buttons, add checkmark on position

--- main.py
+++ main.py
@@ -30,4 +30,13 @@ canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
 
+start_btn = Button(text="Start", bg="white", border=0, width=5, height=2)
+start_btn.grid(column=0, row=2)
+
+checkmark_text = Label(text="✔", font=(FONT_NAME, 12, "bold"), fg=GREEN, bg=YELLOW)
+checkmark_text.grid(column=1, row=3)
+
+reset_btn = Button(text="Reset", bg="white", border=0, width=5, height=2)
+reset_btn.grid(column=2, row=2)
+
 window.mainloop()
